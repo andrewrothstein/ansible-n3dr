426e84a5a0fd8615e103bae2021faa774a1f3e6d
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-01-29 01:11:46 +0000

    k3dr:7.3.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -6,18 +6,18 @@ dl()
 {
     local ver=$1
     local os=$2
-    local url=$MIRROR/$ver/n3dr-${os}.sha512.txt
+    local url=$MIRROR/$ver/n3dr-${os}-latest.sha512.txt
 
-    printf "      # %s\n" $url
-    printf "      %s: sha512:%s\n" $os $(curl -sSL $url | awk '{print $1}')
+    printf "    # %s\n" $url
+    printf "    %s: sha512:%s\n" $os $(curl -sSL $url | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver linux
-    dl $ver osx
+    dl $ver ubuntu
+    dl $ver macos
     dl $ver windows
 }
 
-dl_ver ${1:-3.3.3}
+dl_ver ${1:-7.3.3}
